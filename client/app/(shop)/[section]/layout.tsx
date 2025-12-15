1c5bd8a8fb0a5c1df3e6d006340919e8ce1cbe3a
Serhii Propolov <job.tenshi@gmail.com>
2025-12-15 22:17:41 +0100

    feat: ui update, shadcn init + select, pages logic

---
+++ client/app/(shop)/[section]/layout.tsx
@@ -0,0 +1,8 @@
+export default function layout({ children }: { children: string }) {
+    return (
+        <div>
+            section layout
+            {children}
+        </div>
+    );
+}
--- client/app/(shop)/page.tsx
+++ client/app/(shop)/page.tsx
@@ -9,6 +9,15 @@ import {
 } from "lucide-react";
 import { useState } from "react";
 
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 import { CATEGORIES } from "@/src/shared/data/CATEGORIES";
 import { USA_STATES } from "@/src/shared/data/USA_STATES";
 import { WrapperContainer } from "@/src/shared/wrappers";
@@ -33,37 +42,67 @@ export default function CategoriesSection() {
                         className="px-7 py-4 rounded-l-3xl outline-0  w-full bg-white transition-colors"
                     />
 
-                    <div className="flex">
-                        <select
-                            className="px-3 py-4 outline-0 bg-white w-[70px]"
-                            value={selectedState || ""}
-                            onChange={(e) => {
-                                setSelectedState(e.target.value);
+                    <div className="flex gap-1 bg-white rounded-r-3xl">
+                        <Select
+                            onValueChange={(value) => {
+                                setSelectedState(value);
                                 setSelectedCity("");
                             }}
                         >
-                            <option value="">Select State</option>
-                            {Object.keys(USA_STATES).map((state) => (
-                                <option key={state} value={state}>
-                                    {state}
-                                </option>
-                            ))}
-                        </select>
+                            <SelectTrigger className="w-[100px] bg-white h-full! border-0 rounded-none py-4.5 shadow-none outline-0!">
+                                <SelectValue placeholder="State" />
+                            </SelectTrigger>
 
-                        <select
-                            className="px-3 outline-0 bg-white w-[150px] truncate"
-                            value={selectedCity || ""}
-                            onChange={(e) => setSelectedCity(e.target.value)}
+                            <SelectContent
+                                position="popper"
+                                side="bottom"
+                                sideOffset={4}
+                                className="max-h-[300px] overflow-y-auto"
+                            >
+                                <SelectGroup>
+                                    <SelectLabel>State</SelectLabel>
+
+                                    {Object.keys(USA_STATES).map((state) => (
+                                        <SelectItem key={state} value={state}>
+                                            {state}
+                                        </SelectItem>
+                                    ))}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                        <Select
+                            onValueChange={(value) => {
+                                setSelectedCity(value);
+                            }}
                             disabled={!selectedState}
                         >
-                            <option value="">Select City</option>
-                            {selectedState &&
-                                USA_STATES[selectedState].map((city) => (
-                                    <option key={city} value={city}>
-                                        {city}
-                                    </option>
-                                ))}
-                        </select>
+                            <SelectTrigger className="w-[200px] bg-white h-full! border-0 rounded-none py-4.5 shadow-none">
+                                <SelectValue placeholder="Select a city" />
+                            </SelectTrigger>
+
+                            <SelectContent
+                                position="popper"
+                                side="bottom"
+                                sideOffset={4}
+                                className="max-h-[300px] overflow-y-auto"
+                            >
+                                <SelectGroup>
+                                    <SelectLabel>City</SelectLabel>
+
+                                    {selectedState &&
+                                        USA_STATES[selectedState].map(
+                                            (city) => (
+                                                <SelectItem
+                                                    key={city}
+                                                    value={city}
+                                                >
+                                                    {city}
+                                                </SelectItem>
+                                            )
+                                        )}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
 
                         <button
                             className="flex items-center justify-center gap-2 px-4 py-3.5 font-medium rounded-r-3xl text-white hover:text-white bg-blue-600 hover:bg-blue-700 transition"
@@ -87,7 +126,7 @@ export default function CategoriesSection() {
                     Categories on bemine
                 </h2>
 
-                <div className="grid grid-cols-6 gap-y-6 gap-x-1 justify-items-center">
+                <div className="grid grid-cols-8 gap-y-6 gap-x-1 justify-items-center">
                     {CATEGORIES.map((cat, i) => (
                         <div
                             key={i}
@@ -105,7 +144,7 @@ export default function CategoriesSection() {
                                 <div className="mt-2 flex flex-col rounded-md shadow-[0px_0px_6px_0px_rgba(0,_0,_0,_0.1)] w-full text-[14px] text-left">
                                     <Link
                                         href={"#"}
-                                        className="flex gap-1 items-end p-3 group hover:bg-blue-50/50 font-semibold py-4"
+                                        className="flex gap-1 items-center p-3 group hover:bg-blue-50/50 font-semibold py-4"
                                     >
                                         <ChevronRightIcon className="size-5 group-hover:stroke-blue-600" />
                                         <div className="group-hover:text-blue-600">
@@ -120,7 +159,7 @@ export default function CategoriesSection() {
                                         <Link
                                             href={"#"}
                                             key={idx}
-                                            className="flex gap-1 items-end p-3 group hover:bg-blue-50/50"
+                                            className="flex gap-1 items-center p-3 group hover:bg-blue-50/50"
                                         >
                                             <ChevronRightIcon className="size-5 group-hover:stroke-blue-600" />
                                             <div className="group-hover:text-blue-600">
