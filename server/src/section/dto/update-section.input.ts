3c05b7c31a494200d6bf5a1b4a001cfe1ed09f1b
Serhii Propolov <job.tenshi@gmail.com>
2025-12-22 20:30:20 +0100

    del: server section delete

--- server/src/section/dto/update-section.input.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType, Int, PartialType } from "@nestjs/graphql";
-import { CreateSectionInput } from "./create-section.input";
-
-@InputType()
-export class UpdateSectionInput extends PartialType(CreateSectionInput) {
-    @Field(() => Int)
-    id: number;
-}
